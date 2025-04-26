cd2a38110e888578eff5130e1b1fe0e1212c592a
MohrJonas <jones.m.987654321@gmail.com>
2025-04-26 23:00:14 +0200

    import-profiles not skips already existant profiles

--- libcitadel/citadel/incus/profile.py
+++ libcitadel/citadel/incus/profile.py
@@ -1,4 +1,10 @@
-from subprocess import run, PIPE
+from citadel.utils.subprocess import run_json
+
+from subprocess import run
+from typing import cast, Any
 
 def create_profile(profile_name: str, contents: str) -> None:
     run(["incus", "profile", "create", profile_name], text=True, input=contents, check=True)
+
+def get_profile_names() -> list[str]:
+    return cast(dict[str, Any], run_json(["incus", "profile", "list", "--format", "json"])).keys()
